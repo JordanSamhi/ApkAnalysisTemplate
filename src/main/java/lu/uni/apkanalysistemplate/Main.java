7f564235606e1775b8c1dece7d1052d33b83a49b
Jordan Samhi <35679728+JordanSamhi@users.noreply.github.com>
2023-11-28 16:55:46 +0100

    Add: multiple dex files

--- src/main/java/lu/uni/apkanalysistemplate/Main.java
+++ src/main/java/lu/uni/apkanalysistemplate/Main.java
@@ -71,6 +71,7 @@ public class Main {
 		Options.v().set_output_format(Options.output_format_none);
 		Options.v().set_src_prec(Options.src_prec_apk);
 		Options.v().set_whole_program(true);
+		Options.v().set_process_multiple_dex(true);
 		List<String> dirs = new ArrayList<String>();
 		dirs.add(CommandLineOptions.v().getApk());
 		Options.v().set_process_dir(dirs);
